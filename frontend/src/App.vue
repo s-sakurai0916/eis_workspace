8777f5cff66e0b8f6484e4c7aba3711690945b58
s_sakurai1 <s_sakurai@eiki-infosys.co.jp>
2024-09-06 17:58:58 +0900

    フロントエンド側のたたき台を作成(未完成)

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  img alt="Vue logo" src="./assets/logo.png">
+  <VuetifyBaseWireframe />
+</template>
+
+<script>
+import VuetifyBaseWireframe from './components/app/VuetifyBaseWireframe.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VuetifyBaseWireframe
+  }
+}
+</script>
---
+++ frontend/src/components/app/VuetifyBaseWireframe.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <v-navigation-drawer v-model="drawer" dark>
+        <v-list>
+          <v-list-item v-for="([icon, text], i) in items" :key="i" link>
+            <v-list-item-icon>
+              <v-icon>{{ icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-footer absolute class="font-weight-medium" dark>
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-footer>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    drawer: null,
+    items: [
+      ["mdi-email", "Inbox"],
+      ["mdi-account-supervisor-circle", "Supervisors"],
+      ["mdi-clock-start", "Clock-in"],
+    ],
+  }),
+};
+</script>
